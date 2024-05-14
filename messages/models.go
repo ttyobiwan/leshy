7a369e415bcc0044563781f32b30f779c8457558
Piotr Tobiasz <49806746+tobias-piotr@users.noreply.github.com>
2024-05-14 08:22:43 +0200

    Add data (#4)

--- messages/models.go
+++ messages/models.go
@@ -1,5 +1,7 @@
 package messages
 
+// Message is a representation of the message that will be retrieved from the database,
+// and sent to the final consumer.
 type Message struct {
 	ID   string
 	Data []byte
